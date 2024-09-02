3a208c499d71b22cd9ac71ebf01d2996e266863b
FernandoGutemberg <fernando70sil@gmail.com>
2024-09-01 22:23:17 -0300

    inserindo funcionalidades da geolocalização

--- src/Geolocalizacao.js
+++ src/Geolocalizacao.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
-
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const Geolocalizacao = () => {
     const [location, setLocation] = useState(null);
@@ -41,16 +41,41 @@ const Geolocalizacao = () => {
         }
     };
 
+    const containerStyle = {
+        width: '100%',
+        height: '600px',
+        marginTop: '20px',
+    };
+
+    const center = location ? {
+        lat: location.latitude,
+        lng: location.longitude
+    } : {
+        lat: -3.745,
+        lng: -38.523
+    };
+
     return (
-        <div>
+        <div className="form-geral">
             <h1 className='titulo-principal'>Geolocalização</h1>
-            <Button type="button" onClick={getLocation}>AQUI</Button>
+            <Button type="button" onClick={getLocation}>BUSCAR MEU LOCAL</Button>
             <div className="mt-3">
                 {location ? (
-                    <p>
-                        Latitude: {location.latitude} <br />
-                        Longitude: {location.longitude}
-                    </p>
+                    <div>
+                        <p>
+                            Latitude: {location.latitude} <br />
+                            Longitude: {location.longitude}
+                        </p>
+                        <LoadScript googleMapsApiKey="AIzaSyBQt4VtdKVpG5R1Ernbwe9eTxHnYSWvHjc">
+                            <GoogleMap
+                                mapContainerStyle={containerStyle}
+                                center={center}
+                                zoom={15}
+                            >
+                                <Marker position={center} />
+                            </GoogleMap>
+                        </LoadScript>
+                    </div>
                 ) : (
                     <p>{error || "Clique no botão para que possamos utilizar suas coordenada para uma melhor experiência."}</p>
                 )}
