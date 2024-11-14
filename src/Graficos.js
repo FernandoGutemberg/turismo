7dea6da5cdbdc206a5d755405213dbfd3ba2cd73
FernandoGutemberg <fernando70sil@gmail.com>
2024-11-13 23:02:14 -0300

    estruturando gráficos

--- src/Graficos.js
+++ src/Graficos.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+
+
+
 
 const Graficos = () => {
+  const [dados, setDados] = useState([]);
+
+
   return (
-    <div>
-      <h1>Gráficos</h1>
-      
-    </div>
-  )
+    <ResponsiveContainer>
+      <LineChart>
+        <CartesianGrid />
+        <XAxis />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line />
+
+
+      </LineChart>
+    </ResponsiveContainer>
+
+  );
 };
 
 export default Graficos;
